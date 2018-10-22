00ee81c290a817ee25cab7379474b1e0281cb84d
Manfred Kaul <Manfred.Kaul@gmail.com>
2018-10-22 18:51:02 +0200

    add "use strict"

--- paper-agile.js
+++ paper-agile.js
@@ -11,25 +11,27 @@
 
 ;(function(){
 
+  "use strict";
+
   const questions = [
     // ["männlich","weiblich","divers"],
     // ["Alter: 18-25", "Alter: 26-30", "Alter: 31-40", "Alter: 41-50", "jünger", "älter"],
     // Manifest
     ["Individuen und Interaktionen",  "Prozesse und Werkzeuge"],
     ["Funktionierende Software",  "Umfassende Dokumentation"],
-    // ["Zusammenarbeit mit dem Kunden",  "Vertragsverhandlung"],
-    // ["Reagieren auf Veränderung",  "Befolgen eines Plans"],
-    //
-    // // Gunter Dueck
-    // ["Individuelle Menschenentwicklung", "Versetzung nach Standardstufen und Klassen"],
-    // ["Aktivierung von Selbstwirksamkeitsgefühl", "Prüfungs- und Zeugnisdokumentation"],
-    // ["Folgen von gewecktem Interesse", "Befolgung von Lehrplänen"],
-    // ["Zukunftsfähigkeit der Bildung", "Bewahren klassischer Vorstellungen"],
-    //
-    // // https://digitaleneuordnung.de/blog/agiles-manifest-fuer-unternehmensentwicklung/
-    // ["Konkrete Leistung", "Powerpoint"],
-    // ["(persönliche) Beziehung", "wasserdichter Vertrag"],
-    // ["sich an neue Herausforderungen anpassen können", "einen festen Plan haben"]
+    ["Zusammenarbeit mit dem Kunden",  "Vertragsverhandlung"],
+    ["Reagieren auf Veränderung",  "Befolgen eines Plans"],
+
+    // Gunter Dueck
+    ["Individuelle Menschenentwicklung", "Versetzung nach Standardstufen und Klassen"],
+    ["Aktivierung von Selbstwirksamkeitsgefühl", "Prüfungs- und Zeugnisdokumentation"],
+    ["Folgen von gewecktem Interesse", "Befolgung von Lehrplänen"],
+    ["Zukunftsfähigkeit der Bildung", "Bewahren klassischer Vorstellungen"],
+
+    // https://digitaleneuordnung.de/blog/agiles-manifest-fuer-unternehmensentwicklung/
+    ["Konkrete Leistung", "Powerpoint"],
+    ["(persönliche) Beziehung", "wasserdichter Vertrag"],
+    ["sich an neue Herausforderungen anpassen können", "einen festen Plan haben"]
     // ["", ""],
   ];
 
